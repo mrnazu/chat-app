304c47cdb2c5b02ef5f08523ec5f12c6fc5bbb54
mrnazu <samuelamsalu1998@gmail.com>
2024-05-16 23:08:44 -0700

    #BACKEND we established web server using WebSocket modules

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("hello world");
+use rocket::futures::{SinkExt, StreamExt};
+use rocket_ws::{Channel, WebSocket};
+
+#[rocket::get("/")]
+fn chat(ws: WebSocket) -> Channel<'static> {
+    ws.channel(move |mut stream| Box::pin(async move {
+        while let Some(message) = stream.next().await{
+            let _ = stream.send(message?).await;
+        }
+        Ok(())
+    }))
+}
+
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", rocket::routes![
+            chat
+        ])
+        .launch()
+        .await;
 }
